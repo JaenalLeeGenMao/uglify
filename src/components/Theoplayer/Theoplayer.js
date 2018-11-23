35db6fc83e2478e276eb22eb69616794ad5b6474
Jaenal Leegenmao <jaenal.leegenmao@supersoccer.co.id>
2018-11-23 13:39:59 +0700

    remove screen lock upon unmount

--- src/components/Theoplayer/Theoplayer.js
+++ src/components/Theoplayer/Theoplayer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import history from '../../history';
-import { theoScripts, theoStyle, theoLibraryLocation } from './config'
+import { theoScripts, theoStyle, theoLibraryLocation } from './config';
 import { Arrow, videoPlayer, arrowIcon, AudioButton } from './style';
 
 // let player;
@@ -28,7 +28,7 @@ class Theoplayer extends Component {
   };
 
   static defaultProps = {
-    licenseKey: '',//theoplayer
+    licenseKey: '', //theoplayer
     autoPlay: true,
     allowMutedAutoplay: true,
     className: '',
@@ -37,7 +37,7 @@ class Theoplayer extends Component {
     showBackBtn: true,
     playerBtnImg: 'https://image.flaticon.com/icons/svg/60/60682.svg', //playerArrow
     noPause: false,
-    handleOnVideoLoad: () => { },
+    handleOnVideoLoad: () => {},
     showAudioButton: false
   };
 
@@ -61,22 +61,22 @@ class Theoplayer extends Component {
   handleScriptInject({ scriptTags }) {
     if (scriptTags) {
       scriptTagCount = scriptTags.length;
-      scriptTags.map((tag) => {
+      scriptTags.map(tag => {
         tag.onload = this.handleOnLoad;
-      })
+      });
     }
   }
 
   loadDynamicStyle = () => {
     let existingStyle = true;
-    theoStyle.map((dt) => {
+    theoStyle.map(dt => {
       const el = document.getElementById(dt.id);
       const elExist = el ? true : false;
       existingStyle = existingStyle && elExist;
     });
 
     if (!existingStyle) {
-      theoStyle.map((dt) => {
+      theoStyle.map(dt => {
         const head = document.getElementsByTagName('head')[0];
         const link = document.createElement('link');
         link.id = dt.id;
@@ -87,18 +87,21 @@ class Theoplayer extends Component {
         head.appendChild(link);
       });
     }
-  }
+  };
 
   configTheoPlayer = () => {
     const playerConfig = {
       libraryLocation: theoLibraryLocation,
       ui: {
-        fluid: true,
-      },
+        fluid: true
+      }
     };
-    this.theoPlayer = new window.THEOplayer.Player(this.containerPlayer, playerConfig);
+    this.theoPlayer = new window.THEOplayer.Player(
+      this.containerPlayer,
+      playerConfig
+    );
     return this.theoPlayer;
-  }
+  };
 
   configVideoPlayer = () => {
     this.player.source = {
@@ -110,7 +113,7 @@ class Theoplayer extends Component {
       ],
       textTracks: this.props.theoConfig
     };
-  }
+  };
 
   loadTheoPlayer() {
     const { autoPlay, noPause, allowMutedAutoplay } = this.props;
@@ -125,7 +128,7 @@ class Theoplayer extends Component {
       this.player.play();
     }
     const that = this;
-    this.player.addEventListener('pause', function () {
+    this.player.addEventListener('pause', function() {
       if (noPause) {
         that.player.play();
       }
@@ -134,7 +137,7 @@ class Theoplayer extends Component {
 
   loadDynamicScript = () => {
     let existingScript = true;
-    theoScripts.map((dt) => {
+    theoScripts.map(dt => {
       const el = document.getElementById(dt.id);
       const elExist = el ? true : false;
       existingScript = existingScript && elExist;
@@ -143,7 +146,7 @@ class Theoplayer extends Component {
     if (!existingScript) {
       const scriptCount = theoScripts.length;
       let loadedScriptCount = 0;
-      theoScripts.map((dt) => {
+      theoScripts.map(dt => {
         const script = document.createElement('script');
         script.src = dt.src;
         script.id = dt.id;
@@ -156,9 +159,8 @@ class Theoplayer extends Component {
             //   this.props.handleOnVideoLoad(this.player);
             // }
           }
-
         };
-      })
+      });
     } else {
       this.loadTheoPlayer();
     }
@@ -166,7 +168,13 @@ class Theoplayer extends Component {
   };
 
   loadFullscreenEvent = () => {
-    ['', 'webkit', 'moz', 'ms'].forEach(prefix => document.addEventListener(prefix + 'fullscreenchange', this.handleFullscreen, false));
+    ['', 'webkit', 'moz', 'ms'].forEach(prefix =>
+      document.addEventListener(
+        prefix + 'fullscreenchange',
+        this.handleFullscreen,
+        false
+      )
+    );
   };
 
   handleFullscreen = () => {
@@ -187,7 +195,10 @@ class Theoplayer extends Component {
   }
 
   componentWillUnmount() {
-    this.player.destroy();
+    if (this.player) {
+      window.screen.orientation.unlock();
+      this.player.destroy();
+    }
   }
 
   handleAudioBtn = () => {
@@ -203,12 +214,11 @@ class Theoplayer extends Component {
     const { toggleArrow, isMuted } = this.state;
     const { className, showBackBtn, showAudioButton } = this.props;
     return (
-
       <div
         className={`${videoPlayer} ${className} video-container video-js theoplayer-skin`}
         onMouseEnter={this.getToggleArrow}
         onMouseLeave={this.getToggleArrow}
-        ref={(el) => {
+        ref={el => {
           this.containerPlayer = el;
         }}
       >
@@ -217,12 +227,12 @@ class Theoplayer extends Component {
             <span className={arrowIcon} />
           </Arrow>
         )}
-        {showAudioButton &&
+        {showAudioButton && (
           <AudioButton onClick={this.handleAudioBtn}>
             {/* <span className={isMuted ? audioMutedIcon : audioIcon} /> */}
             {isMuted ? 'MUTED' : 'SOUND'}
           </AudioButton>
-        }
+        )}
       </div>
     );
   }
